59e9b717d02f147f3d13285d3166f36f5157e1cd
nick-korres <nickorres@hotmail.com>
2020-04-18 18:46:52 +0300

    Database url from enviroment variable

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+from os import environ
 
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '\xc8\xc9i\x0c\x94\xc5\rM\xb6!1\xe0\x15\xbb\xc0'
+    SECRET_KEY = environ.get("KEY")
     CORS_HEADERS = 'Content-Type'
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgres://nick:nick2525@localhost:5432/city_guess'
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
     DEBUG = True
 
 
