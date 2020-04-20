d2454f672e597c4e5eda5314c9ce3d0fd30b5704
nick-korres <nickorres@hotmail.com>
2020-04-20 15:26:28 +0300

    added migration scripts

--- manage.py
+++ manage.py
@@ -5,9 +5,12 @@ from flask_migrate import Migrate, MigrateCommand
 from app import app, db
 from models import Cities
 import os
+from dotenv import load_dotenv
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 migrate = Migrate(app, db)
 
 migrate = Migrate(app, db)
