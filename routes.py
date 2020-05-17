bc1d302ba26da2ddadb7f4ed9abda11670fb6cf4
nick-korres <nickorres@hotmail.com>
2020-05-17 20:36:40 +0300

    changed routes to only accept GET

--- routes.py
+++ routes.py
@@ -13,7 +13,7 @@ def welcome():
     return "Hello There"
 
 
-@images.route('/images/')
+@images.route('/images/' , methods=['GET'])
 @cross_origin()
 def get_cities():
     temp = Cities.query.all()
@@ -21,10 +21,9 @@ def get_cities():
     return Response(result, 200)
 
 
-@images.route('/images/<name>')
+@images.route('/images/<name>', methods=['GET'])
 def show_image(name):
     
     correct_path = images.static_url_path+'/images/'
     correct_path = os.path.abspath(os.path.dirname(__file__))+correct_path
-    print(correct_path)
     return send_from_directory(correct_path, filename=name)
